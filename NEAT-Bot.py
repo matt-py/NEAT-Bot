8b35d8c0cb22735f4cf2ca984142ef13cc0bde3b
Matt <48388592+matt-py@users.noreply.github.com>
2019-03-17 02:02:09 +0000

    Update to use NEAT algorithm
    Updated main NEAT-Bot.py file to utilise the NEAT files and algorithm. Not tested.

--- NEAT-Bot.py
+++ NEAT-Bot.py
@@ -1,4 +1,13 @@
 import math
+import random
+
+# from connection_gene import ConnectionGene
+# from connection_history import ConnectionHistory
+# from genome import Genome
+# from node import Node
+# from player import Player
+from population import Population
+# from species import Species
 
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
@@ -7,34 +16,70 @@ from rlbot.utils.structures.game_data_struct import GameTickPacket
 class NEATBot(BaseAgent):
 
     def initialize_agent(self):
-        #This runs once before the bot starts up
+        # This runs once before the bot starts up
         self.controller_state = SimpleControllerState()
 
+        # NEAT stuff
+        self.population = Population(10)
+        self.bot_score = 0
+
+
+
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
-        ball_location = Vector2(packet.game_ball.physics.location.x,
-                                packet.game_ball.physics.location.y)
+        if packet.game_cars[self.index].score_info.score > self.bot_score:
+            reward = packet.game_cars[self.index].score_info.score - self.bot_score
+            self.bot_score = packet.game_cars[self.index].score_info.score
+            
+        self.reset_controller_state()
 
-        my_car = packet.game_cars[self.index]
-        car_location = Vector2(my_car.physics.location.x, my_car.physics.location.y)
-        car_direction = get_car_facing_vector(my_car)
-        car_to_ball = ball_location - car_location
+        inputs = self.get_inputs(packet)
 
-        steer_correction_radians = car_direction.correction_to(car_to_ball)
+        if self.population.done():
+            self.population.natural_selection()
 
-        if steer_correction_radians > 0:
-            # Positive radians in the unit circle is a turn to the left.
-            turn = -1.0  # Negative value for a turn to the left.
-            action_display = "turn left"
-        else:
-            turn = 1.0
-            action_display = "turn right"
-
-        self.controller_state.throttle = 1.0
-        self.controller_state.steer = turn
-
-        draw_debug(self.renderer, my_car, packet.game_ball, action_display)
+        choice = self.population.update_alive(inputs, reward)
+        self.set_controller_state(choice)
 
         return self.controller_state
+    
+    def reset_controller_state(self):
+        self.controller_state.throttle = 0
+        self.controller_state.steer = 0
+        self.controller_state.pitch = 0
+        self.controller_state.yaw = 0
+        self.controller_state.roll = 0
+        self.controller_state.jump = 0
+        self.controller_state.boost = 0
+        self.controller_state.handbrake = 0
+
+    def set_controller_state(self, choice):
+        if choice == 0:
+            self.controller_state.throttle = 1
+        elif choice == 1:
+            self.controller_state.throttle = 1
+            self.controller_state.steer = 1
+        elif choice == 2:
+            self.controller_state.throttle = 1
+            self.controller_state.steer = -1
+        else:
+            print("UNEXPECTED: unknown choice")
+
+
+    def get_inputs(self, packet):
+        ball = Vector2(packet.game_ball.physics.location.x,
+                        packet.game_ball.physics.location.y)
+        ball_speed = Vector2(packet.game_ball.physics.velocity.x,
+                        packet.game_ball.physics.velocity.y).mag()
+        my_car = packet.game_cars[self.index]
+        bot = Vector2(my_car.physics.location.x, my_car.physics.location.y)
+        bot_speed = Vector2(my_car.physics.velocity.x, my_car.physics.velocity.y).mag()
+        bot_dir = get_car_facing_vector(my_car)
+        car_to_ball = ball - bot
+        angle_to_ball = bot_dir.correction_to(car_to_ball)
+
+        input_array = [bot.x, bot.y, bot_speed, ball.x, ball.y, ball_speed, bot_dir, angle_to_ball]
+        return input_array
+        
 
 class Vector2:
     def __init__(self, x=0, y=0):
@@ -47,20 +92,18 @@ class Vector2:
     def __sub__(self, val):
         return Vector2(self.x - val.x, self.y - val.y)
 
+    def mag(self):
+        return math.sqrt((self.x**2) + (self.y**2))
+
     def correction_to(self, ideal):
-        # The in-game axes are left handed, so use -x
         current_in_radians = math.atan2(self.y, -self.x)
         ideal_in_radians = math.atan2(ideal.y, -ideal.x)
-
         correction = ideal_in_radians - current_in_radians
-
-        # Make sure we go the 'short way'
         if abs(correction) > math.pi:
             if correction < 0:
                 correction += 2 * math.pi
             else:
                 correction -= 2 * math.pi
-
         return correction
 
 
@@ -73,10 +116,3 @@ def get_car_facing_vector(car):
 
     return Vector2(facing_x, facing_y)
 
-def draw_debug(renderer, car, ball, action_display):
-    renderer.begin_rendering()
-    # draw a line from the car to the ball
-    renderer.draw_line_3d(car.physics.location, ball.physics.location, renderer.white())
-    # print the action that the bot is taking
-    renderer.draw_string_3d(car.physics.location, 2, 2, action_display, renderer.white())
-    renderer.end_rendering()
